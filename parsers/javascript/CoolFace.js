0eae407a6f39c74e28931bf4081e89b6d7474846
Brandon Ryall <bryall@sevone.com>
2014-02-19 18:12:23 -0500

    Added some more stuff for starting

--- parsers/javascript/CoolFace.js
+++ parsers/javascript/CoolFace.js
@@ -3,3 +3,14 @@
  * CoolFace JS parser
  *
  **/
+
+(function(global) {
+	"use strict";
+	var CoolFace = function() {
+		this.parse = function( cf ) {
+			console.log(cf);
+		};
+	};
+	var c = new CoolFace();
+	global.CoolFace = c;
+})(window);
