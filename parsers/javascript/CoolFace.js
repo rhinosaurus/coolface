df902f6d47d23c41b2e82675b9dca1be7d826039
Brandon Ryall <bryall@sevone.com>
2014-02-19 19:30:10 -0500

    CoolFace javascript parse version 0.1

--- parsers/javascript/CoolFace.js
+++ parsers/javascript/CoolFace.js
@@ -8,7 +8,20 @@
 	"use strict";
 	var CoolFace = function() {
 		this.parse = function( cf ) {
-			console.log(cf);
+			var output = {};
+			cf = cf.replace( /(\(:)/, '');
+			cf = cf.replace( /(:\))/, '');
+			var result, exR,
+				r = new RegExp( "([^\t]+)", "g" ),
+				n = new RegExp( "(.*)(.=\\).)(.*)" );
+			while( (result = r.exec(cf)) !== null ) {
+				exR = n.exec( result[0] );
+				if( exR !== null ) {
+					output[ exR[1] ] = exR[3];
+				}
+			}
+
+			return output;
 		};
 	};
 	var c = new CoolFace();
