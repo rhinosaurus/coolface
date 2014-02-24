2c790617c52944ed0426bd61ea7c574b958a1fef
Ralph Mancinelli <ralfizzle@gmail.com>
2014-02-24 13:19:26 -0500

    Added type detection, a lot of debug code in place.

--- parsers/php/CoolFace.php
+++ parsers/php/CoolFace.php
@@ -11,22 +11,48 @@
 class CoolFace {
 
 	// Check for special characters and return type.
-	protected function getType( $line ) {
+	protected function getType( $value ) {
+		switch( $value[0] ) {
+			case "#":
+				return "integer";
+				break;
+			case "!":
+				return "boolean";
+				break;
+		}
 
+		switch( $value[0].$value[1] ) {
+			case "(:":
+				return "object";
+				break;
+			case "[:":
+				return "array";
+				break;
+		}
+
+		return "string";
 	}
 
 	public static function coolface_encode( $coolData ) {
-		return $coolData
+		return $coolData;
 	}
 
 	public static function coolface_decode( $coolData ) {
 
-		$pattern = '/([\s\S]+)\s=\)\s([\s\S]+)/';
+		$pattern = '/\t+([\s\S]+)\s=\)\s([\s\S]+)/';
+
+		$lines = explode( "\n", $coolData );
+
+		print_r($lines);
 
 		$matches = [];
-		preg_match_all( $pattern, $coolData, $matches );
+		foreach( $lines as $line ) {
+			preg_match_all( $pattern, $line, $matches );
 
-		print_r($matches);
+			echo static::getType( current( $matches[2] ) ).PHP_EOL;
+
+			print_r($matches);
+		}		
 
 		return $coolData;
 	}
@@ -34,3 +60,4 @@ class CoolFace {
 }
 
 ?>
+cd
